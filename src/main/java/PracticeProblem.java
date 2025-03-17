593a74bdc0b9cb749fd846b83c3a972e510662d9
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-03-17 14:44:27 +0000

    :)

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,47 @@ public class PracticeProblem {
 
 	}
 
+	public static int lettersToFive(String word, char letter){
+		int index = 0;
+		int count = 0;
+		while (index < word.length() && count < 5){
+			if (word.charAt(index) == letter){
+				count++;
+			}
+			index++;
+		}
+		if (count == 5){
+			return index;
+		}else{
+			return -1;
+		}
+	}
+
+	public static int countLetter(String word, char letter){
+		int index = 0;
+		int count = 0;
+		while (index < word.length()){
+			if (word.charAt(index) == letter){
+				count++;
+			}
+			index++;
+		}
+		return count;
+	}
+
+
+	public static int oddSum (int num, int num2){
+		int smallNum = Math.min(num, num2);
+		int bigNum = Math.max(num, num2);
+		int sum = 0;
+		while (smallNum <= bigNum){
+			if (smallNum % 2 != 0){
+				sum += smallNum;
+				}
+				smallNum++;
+			}
+			return sum;
+		}		
 }
+
+
